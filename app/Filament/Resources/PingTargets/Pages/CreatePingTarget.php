8abf189609afa1c6e0d616a17881bda2c509a298
NodoBTCboricua <carcan00725@proton.me>
2026-02-21 19:53:51 -0400

    Refactor ping monitoring feature to improve code structure and enhance readability

---
+++ app/Filament/Resources/PingTargets/Pages/CreatePingTarget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PingTargets\Pages;
+
+use App\Filament\Resources\PingTargets\PingTargetResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePingTarget extends CreateRecord
+{
+    protected static string $resource = PingTargetResource::class;
+}
---
+++ app/Filament/Resources/PingTargets/Pages/EditPingTarget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PingTargets\Pages;
+
+use App\Filament\Resources\PingTargets\PingTargetResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPingTarget extends EditRecord
+{
+    protected static string $resource = PingTargetResource::class;
+}
